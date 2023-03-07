46ee6cd9488ef65017ba7b9f7e453d840ba1a095
reisir <reisirito@gmail.com>
2023-03-07 07:36:32 +0200

    parse MonD.inc from skin repo

--- models/Skin.js
+++ models/Skin.js
@@ -6,6 +6,7 @@ const SkinSchema = mongoose.Schema(
     skin_name: { type: String, required: true },
     full_name: { type: String, required: true, unique: true },
     topics: [String],
+    description: String,
     latest_release: {
       tag_name: String,
       browser_download_url: String,
@@ -16,11 +17,15 @@ const SkinSchema = mongoose.Schema(
       avatar_url: String,
     },
   },
-  { timestamps: true }
+  { timestamps: true, strict: true, strictQuery: true }
 );
 
 SkinSchema.virtual("version").get(function () {
   return this.latest_release.tag_name;
+});
+
+SkinSchema.virtual("owner.github").get(function () {
+  return `https://github.com/${this.full_name.split("/")[0]}`;
 });
 
 SkinSchema.plugin(require("mongoose-lean-virtuals"));
--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,16 +1,98 @@
 const express = require("express");
 const Router = express.Router();
 
-// https://docs.github.com/en/webhooks-and-events/webhooks/webhook-events-and-payloads
-// TODO: handle payload.action == ping
-// TODO: handle payload.action == release
+// Models
+const mongoose = require("mongoose");
+const Skin = mongoose.model("skin");
+
+// Dependencies
+const ini = require("ini");
+const axios = require("axios").create({ baseURL: "https://api.github.com" });
+const headers = (raw = false) => ({
+  Accept: `application/vnd.github${raw ? ".raw" : "+json"}`,
+  Authentication: `Bearer ${process.env.GITHUB_PAT}`,
+  "X-GitHub-Api-Version": "2022-11-28",
+});
+
+// MonD.inc options to Skin schema paths
+const incOptionToSkinPath = {
+  SkinName: "skin_name",
+  Author: "owner.name",
+  ProfilePicture: "owner.avatar_url",
+  Description: "description",
+};
 
 Router.post("/webhooks/github", async function (req, res, next) {
   const { body: payload } = req;
 
-  console.log(payload.action);
-  console.log(payload.repository);
-  console.log(payload.release);
+  switch (payload.action) {
+    case "ping": {
+      const updatedSkin = await updateSkin(payload);
+      const latestRelease = await updateRelease(payload);
+      return res.json({ updatedSkin, latestRelease });
+    }
+    case "released": {
+      console.log(payload.release);
+    }
+    default: {
+      console.log(payload.action);
+    }
+  }
 });
+
+async function updateSkin(payload) {
+  const { repository } = payload;
+  const { full_name } = repository;
+  const {
+    owner: { login, avatar_url },
+  } = repository;
+
+  // Get MonD.inc
+  const inc = await getMondInc(full_name);
+  // Get the override values from MonD.inc
+  const overrides = incOverrides(inc);
+
+  if (!overrides.skin_name) throw Error("MonD.inc did not contain SkinName");
+
+  // Update or upsert skin with new info
+  await Skin.findOneAndUpdate(
+    { full_name },
+    { ...repository, owner: { name: login, avatar_url } },
+    { upsert: true }
+  );
+
+  // Override github API values with values from MonD.inc
+  const updatedSkin = await Skin.findOneAndUpdate({ full_name }, overrides, {
+    new: true,
+  });
+  return updatedSkin;
+}
+
+async function updateRelease(payload) {
+  console.error("Implement updateRelease()");
+  return {};
+}
+
+function incOverrides(inc) {
+  const overrides = {};
+  for (const [option, key] of Object.entries(incOptionToSkinPath)) {
+    if (inc[option]) overrides[key] = inc[option];
+  }
+  return overrides;
+}
+
+async function getMondInc(full_name) {
+  const response = await axios({
+    method: "GET",
+    url: `/repos/${full_name}/contents/@Resources/MonD.inc`,
+    headers: headers(true),
+  });
+  const data = response.data;
+  if (!data) throw Error("No MonD.inc found");
+  const inc = ini.parse(data);
+  const section = inc["MonD"];
+  if (!section) throw Error("No [MonD] section in MonD.inc");
+  return section;
+}
 
 module.exports = Router;
