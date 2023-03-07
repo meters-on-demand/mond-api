e7866a44a467ee89209f90be2bb3db4486b1cca2
reisir <reisirito@gmail.com>
2023-03-07 04:42:58 +0200

    no need for nested documents

--- models/Skin.js
+++ models/Skin.js
@@ -1,24 +1,20 @@
 const mongoose = require("mongoose");
 
-const ReleaseSchema = mongoose.Schema({
-  tag_name: String,
-  browser_download_url: String,
-  name: String,
-});
-
-const GithubUser = mongoose.Schema({
-  name: String,
-  avatar_url: String,
-});
-
 const SkinSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
     skin_name: { type: String, required: true },
     full_name: { type: String, required: true, unique: true },
     topics: [String],
-    latest_release: ReleaseSchema,
-    owner: GithubUser,
+    latest_release: {
+      tag_name: String,
+      browser_download_url: String,
+      name: String,
+    },
+    owner: {
+      name: String,
+      avatar_url: String,
+    },
   },
   { timestamps: true }
 );
