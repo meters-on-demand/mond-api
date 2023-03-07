bfc52fb5baf59bf39e0101d4f4d906d17f6622ac
reisir <reisirito@gmail.com>
2023-03-07 16:55:24 +0200

    update release information

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -24,25 +24,19 @@ const incOptionToSkinPath = {
 
 Router.post("/webhooks/github", async function (req, res, next) {
   const { body: payload } = req;
-
-  switch (payload.action) {
-    case "ping": {
-      const updatedSkin = await updateSkin(payload);
-      const latestRelease = await updateRelease(payload);
-      return res.json({ updatedSkin, latestRelease });
-    }
-    case "released": {
-      console.log(payload.release);
-    }
-    default: {
-      console.log(payload.action);
-    }
+  acceptedActions = ["ping", "released"];
+  if (acceptedActions.includes(payload.action)) {
+    if (!payload.full_name)
+      throw Error("payload.repository does not contain full_name");
+    await updateSkin(payload);
+    await updateRelease(payload);
   }
+  return res.end("OK");
 });
 
 async function updateSkin(payload) {
-  const { repository } = payload;
-  const { full_name } = repository;
+  const { full_name } = payload.repository;
+  const repository = await getRepoInformation(full_name);
   const {
     owner: { login, avatar_url },
   } = repository;
@@ -51,7 +45,6 @@ async function updateSkin(payload) {
   const inc = await getMondInc(full_name);
   // Get the override values from MonD.inc
   const overrides = incOverrides(inc);
-
   if (!overrides.skin_name) throw Error("MonD.inc did not contain SkinName");
 
   // Update or upsert skin with new info
@@ -69,16 +62,44 @@ async function updateSkin(payload) {
 }
 
 async function updateRelease(payload) {
-  console.error("Implement updateRelease()");
-  return {};
+  const { full_name } = payload.repository;
+  const latest_release = await getLatestRelease(full_name);
+
+  if (!latest_release.assets)
+    throw Error(`${full_name} latest release contains no assets`);
+
+  const rmskin = latest_release.assets.find((asset) =>
+    asset.name.match(/.*\.rmskin$/)
+  );
+  latest_release.browser_download_url = rmskin.browser_download_url;
+
+  const updatedSkin = await Skin.findOneAndUpdate(
+    { full_name },
+    { latest_release },
+    { new: true }
+  );
+
+  return updatedSkin;
 }
 
-function incOverrides(inc) {
-  const overrides = {};
-  for (const [option, key] of Object.entries(incOptionToSkinPath)) {
-    if (inc[option]) overrides[key] = inc[option];
-  }
-  return overrides;
+async function getLatestRelease(full_name) {
+  const response = await axios({
+    method: "GET",
+    url: `/repos/${full_name}/releases/latest`,
+    headers: headers(),
+  });
+  const data = response.data;
+  return data;
+}
+
+async function getRepoInformation(full_name) {
+  const response = await axios({
+    method: "GET",
+    url: `/repos/${full_name}`,
+    headers: headers(),
+  });
+  const data = response.data;
+  return data;
 }
 
 async function getMondInc(full_name) {
@@ -93,6 +114,23 @@ async function getMondInc(full_name) {
   const section = inc["MonD"];
   if (!section) throw Error("No [MonD] section in MonD.inc");
   return section;
+}
+
+function incOverrides(inc) {
+  const overrides = {};
+  for (const [option, key] of Object.entries(incOptionToSkinPath)) {
+    if (inc[option]) overrides[key] = stripQuotes(inc[option]);
+  }
+  return overrides;
+}
+
+function stripQuotes(s) {
+  const strippable = ["'", '"'];
+  if (strippable.includes(s.at(0)) && strippable.includes(s.at(-1))) {
+    s = s.slice(1, -1);
+  }
+  s = s.trim();
+  return s;
 }
 
 module.exports = Router;
